86f623bac3ee28f0fbb9c27d0c65e3ba0e98a0ca
PiotrTrybowski <37082715+PiotrTrybowski@users.noreply.github.com>
2019-06-24 22:18:04 +0200

    Update hello_cluster.py
    Working and outputting nicely formatted data

--- hello_cluster.py
+++ hello_cluster.py
@@ -15,7 +15,10 @@ df = spark.read.csv('s3://piotrtrybowski/200705hourly.csv', header=True)
 #df[' Dry Bulb Temp'].show()
 # counts = data.map(lambda x: (x, 1)).reduceByKey(add).sortBy(lambda x: x[1], ascending=False).coalesce(1).saveAsTextFile('s3://piotrtrybowski/output.txt')
 average_all = df.select(f.avg(' Dry Bulb Temp'),f.avg(' Wet Bulb Temp'),f.avg(' Wind Speed (kt)')).show()
-average_daily = df.groupBy(' YearMonthDay').agg({' Dry Bulb Temp':"avg"}).show()
+average_daily = df.groupBy(' YearMonthDay')\
+    .agg({' Dry Bulb Temp':"avg",' Wet Bulb Temp':"avg",' Wind Speed (kt)':"avg"})\
+    .sort(' YearMonthDay')\
+    .coalesce(1).write.option("header","true").csv('s3://piotrtrybowski/outputs/hourly_to_daily_avg.csv')
 
 
 sc.stop()
